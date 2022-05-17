4de9c971822c59f994d7c7632049532398e579c2
Zac <68226299+tucaza555@users.noreply.github.com>
2022-05-18 01:14:29 +1000

    Update myApp.js
    started a working express server and served a html file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello, World!");
 
+/*app.get("/", function(req, res) {
+  res.send("Hello Express");
+});*/
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
