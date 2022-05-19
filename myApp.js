66ac1bd37915360ffee0edcb2e20b7047430d323
Zac <68226299+tucaza555@users.noreply.github.com>
2022-05-19 13:18:23 +1000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,11 +7,35 @@ console.log("Hello, World!");
   res.send("Hello Express");
 });*/
 
-app.get("/", function(req, res) {
+/*app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
+});*/
+
+/*app.use("/public", express.static(__dirname + "/public"));*/
+
+/*app.get("/json", (req, res) => {
+  if (process.env['MESSAGE_STYLE'] == "uppercase") {
+    res.json({message: "Hello json".toUpperCase()});
+  } else {A
+    res.json({message: "Hello json"});
+  }
+});*/
+
+/*app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});*/
+
+app.get('/user', function(req, res, next) {
+  req.user = getTheUserSync();  // Hypothetical synchronous operation
+  next();
+}, function(req, res) {
+  res.send(req.user);
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+
+
+
 
 
 
